e76c8e062765c06e6525b4fe829e3f616b34c2c3
Termitehuegel <64739941+termitehuegel@users.noreply.github.com>
2021-08-05 12:58:03 +0200

    Comments and Documentation, quickSort can now be called without specification of begin and end.

--- src/de/termitehuegel/QuickSort/Main.java
+++ src/de/termitehuegel/QuickSort/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         String[] s = {"zz", "aa", "xw", "lk", "k", "la"};
         QuickSort<String> quickSort = new QuickSort<>();
-        quickSort.quickSort(s, 0, s.length-1);
+        quickSort.quickSort(s);
         System.out.println(Arrays.toString(s));
     }
 }
--- src/de/termitehuegel/QuickSort/QuickSort.java
+++ src/de/termitehuegel/QuickSort/QuickSort.java
@@ -5,6 +5,15 @@ package de.termitehuegel.QuickSort;
  */
 public class QuickSort<T extends Comparable<T>> {
 
+    public void quickSort(T[] array) {
+        quickSort(array, 0, array.length-1);
+    }
+    /**
+     * Sorts an array
+     * @param array that will be sorted
+     * @param begin index of the part that will be sorted
+     * @param end index of the part that will be sorted
+     */
     public void quickSort(T[] array ,int begin, int end) {
         //finished if less than 2 Elements need to be sorted (-> 1 or 0)
         if (begin < end) {
@@ -18,6 +27,13 @@ public class QuickSort<T extends Comparable<T>> {
         }
     }
 
+    /**
+     * Sorts all lower elements left and all bigger right of the pivot
+     * @param array the array that is sorted
+     * @param begin the index from the beginning of the part that is to be sorted
+     * @param end the end index of the part that is to be sorted
+     * @return the index of the pivot element
+     */
     private int findPivot(T[] array, int begin, int end) {
         //The last element shall be the pivot
         T pivot = array[end];
@@ -42,6 +58,12 @@ public class QuickSort<T extends Comparable<T>> {
         return lastLowerThanPivot+1;
     }
 
+    /**
+     * Swaps the positions of the values at e1 and e2
+     * @param array the array e1 and e2 are part of
+     * @param e1 the first elements index
+     * @param e2 the second elements index
+     */
     private void swap(T[] array, int e1, int e2) {
         T temp = array[e1];
         array[e1] = array[e2];
